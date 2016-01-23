4322bbbb9ccf1682cbe776dc3215bb053910feb6
Matt Bentley <matt.bentley@docker.com>
2016-01-22 20:22:29 -0500

    Update to add http2 and updated PAM module
    Signed-off-by: Matt Bentley <matt.bentley@docker.com>

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ To pull this image:
 `docker pull mbentley/nginx-build`
 
 To run this image:
-`docker run --rm mbentley/docker-nginx-build`
+`docker run -it --rm mbentley/docker-nginx-build`
 
 Optional - Use a volume:
 
-`docker run --rm -v /data/nginx:/data mbentley/docker-nginx-build`
+`docker run -it --rm -v /data/nginx:/data mbentley/nginx-build`
--- build.sh
+++ build.sh
@@ -23,35 +23,36 @@ fi
 
 if [ ! -d nginx-dav-ext-module ]
 then
-  git clone https://github.com/arut/nginx-dav-ext-module.git
+  git clone --depth 1 https://github.com/arut/nginx-dav-ext-module.git
 fi
 
 if [ ! -d ngx_http_auth_pam_module-1.2 ]
 then
-  wget http://web.iti.upv.es/~sto/nginx/ngx_http_auth_pam_module-1.2.tar.gz
-  tar zxf ngx_http_auth_pam_module-1.2.tar.gz
-  rm ngx_http_auth_pam_module-1.2.tar.gz
+  wget https://github.com/stogh/ngx_http_auth_pam_module/archive/v1.4.tar.gz
+  tar zxf v1.4.tar.gz
+  rm v1.4.tar.gz
 fi
 
 ./auto/configure \
---sbin-path=/usr/local/sbin \
---conf-path=/etc/nginx/nginx.conf \
---error-log-path=/var/log/nginx/error.log \
---pid-path=/var/run/nginx.pid \
---lock-path=/var/lock/nginx.lock \
---http-log-path=/var/log/nginx/access.log \
---with-http_dav_module \
---add-module=${WORKDIR}/nginx-dav-ext-module \
---http-client-body-temp-path=/var/lib/nginx/body \
---with-http_ssl_module \
---with-http_realip_module \
---http-proxy-temp-path=/var/lib/nginx/proxy \
---with-http_stub_status_module \
---http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
---with-http_auth_request_module \
---add-module=${WORKDIR}/ngx_http_auth_pam_module-1.2 \
---user=www-data \
---group=www-data
+  --sbin-path=/usr/local/sbin \
+  --conf-path=/etc/nginx/nginx.conf \
+  --error-log-path=/var/log/nginx/error.log \
+  --pid-path=/var/run/nginx.pid \
+  --lock-path=/var/lock/nginx.lock \
+  --http-log-path=/var/log/nginx/access.log \
+  --with-http_dav_module \
+  --add-module=${WORKDIR}/nginx-dav-ext-module \
+  --http-client-body-temp-path=/var/lib/nginx/body \
+  --with-http_ssl_module \
+  --with-http_realip_module \
+  --http-proxy-temp-path=/var/lib/nginx/proxy \
+  --with-http_stub_status_module \
+  --http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
+  --with-http_auth_request_module \
+  --add-module=${WORKDIR}/ngx_http_auth_pam_module-1.4 \
+  --with-http_v2_module \
+  --user=www-data \
+  --group=www-data
 
 make
 
